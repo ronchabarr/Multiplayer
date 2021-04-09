bef5f3d403bc2c355180cdabedca06d26d8f27b1
ronchabarr <ronchabarr@gmail.com>
2021-04-09 18:08:26 +0300

    Initial commit

---
+++ Assets/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour
+{
+    float count=0;
+    public float speed = 10.0F;
+    public float rotationSpeed = 100.0F;
+   internal Rigidbody rb;
+    Vector2 MovementVector;
+    float rot;
+    private void Start()
+    {
+        
+    }
+    // Update is called once per frame
+    void Update()
+    {if (rb.IsSleeping()) 
+        rb.WakeUp();
+        float translation = Input.GetAxis("Vertical") * speed;
+        float rotation = Input.GetAxis("Horizontal") * rotationSpeed;
+        MovementVector = new Vector2(translation, rotation);
+        
+    }
+    private void FixedUpdate()
+    {
+        rot += MovementVector.y;
+        rb.AddRelativeForce(0, 0, MovementVector.x);
+        rb.MoveRotation(Quaternion.Euler(0, rot, 0));
+        count++;
+        Debug.Log(MovementVector.y);
+       
+    }
+}
---
+++ Assets/SetupLocalPlayer.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class SetupLocalPlayer : NetworkBehaviour
+{
+    [SerializeField] GameObject FpsCam;
+    [SerializeField] Transform camPlace;
+    PlayerController playerController;
+
+    Canvas canvas;
+    Camera camera;
+    // Start is called before the first frame update
+    void Start()
+    {
+        playerController = GetComponent<PlayerController>();
+
+        if (isLocalPlayer)
+        {
+            playerController.rb = GetComponent<Rigidbody>();
+
+            gameObject.GetComponent<PlayerController>().enabled = true;
+            Instantiate(FpsCam, camPlace);
+        }
+        else
+        {
+            gameObject.GetComponent<PlayerController>().enabled = false;
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
